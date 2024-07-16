3027f0a87abe36b3c35991e6cc1ee5abd7c75317
kitchan98 <81081193+kitchan98@users.noreply.github.com>
2024-07-16 20:57:45 +0100

    bug fixed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
 import Home from './pages/Home';
 import PostDataNeed from './pages/PostDataNeed';
 import RegisterFreelancer from './pages/RegisterFreelancer';
+import './styles/global.css';
 
-const App = () => (
-  <Router basename="/DatumZ/datumz">
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/post-data-need" element={<PostDataNeed />} />
-      <Route path="/register-freelancer" element={<RegisterFreelancer />} />
-      {/* Add other routes here */}
-    </Routes>
-  </Router>
-);
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/post-data-need" element={<PostDataNeed />} />
+            <Route path="/register-freelancer" element={<RegisterFreelancer />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
+}
 
 export default App;
