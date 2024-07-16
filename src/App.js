fe8d733cf177dc5fb6090b49429ee88335123ea7
kitchan98 <81081193+kitchan98@users.noreply.github.com>
2024-07-16 20:52:01 +0100

    fixed directory issues

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
 import Home from './pages/Home';
 import PostDataNeed from './pages/PostDataNeed';
 import RegisterFreelancer from './pages/RegisterFreelancer';
-import './styles/global.css';
-
 
 const App = () => (
-  <Router basename="/dataumz">
+  <Router basename="/DatumZ/datumz">
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/post-data-need" element={<PostDataNeed />} />
       <Route path="/register-freelancer" element={<RegisterFreelancer />} />
+      {/* Add other routes here */}
     </Routes>
   </Router>
 );
