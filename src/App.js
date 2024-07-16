a5c9b956e8633f8b1ee9ee041dff9ee68c2516a5
kitchan98 <81081193+kitchan98@users.noreply.github.com>
2024-07-16 20:45:33 +0100

    Fix routing and add Babel plugin

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import PostDataNeed from './pages/PostDataNeed';
 import RegisterFreelancer from './pages/RegisterFreelancer';
 import './styles/global.css';
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/post-data-need" element={<PostDataNeed />} />
-            <Route path="/register-freelancer" element={<RegisterFreelancer />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
-  );
-}
+
+const App = () => (
+  <Router basename="/dataumz">
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/post-data-need" element={<PostDataNeed />} />
+      <Route path="/register-freelancer" element={<RegisterFreelancer />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
